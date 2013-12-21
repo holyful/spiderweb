da587a6a500313ddb0529281ea05f059606562cf
holyful <holyful@hotmail.com>
2013-12-22 02:55:08 +0800

    ＊ 全部读写使用流，提升效率 ＊ 使用cluster提高并发效率

--- core/main.js
+++ core/main.js
@@ -13,94 +13,117 @@ var http = require('http'),
     fs = require('fs'),
     spawnCount = 0,
     requestQueue = [],
+    date = null,
+    cluster = require('cluster'),
+    numWks = configs.concurrentWorkers;
     date = new Date();
 
-
-var server = http.createServer(function (req, res) {
-
-    var targetQuery = url.parse(req.url).query ? url.parse(req.url).query : null,
-        targetUrl = null, resData = null;
-    if(targetQuery){
-        targetUrl = queryString.parse(targetQuery).targetUrl;
+if (cluster.isMaster) {
+    //创建集群
+    for (var i = 0; i < numWks; i++) {
+        cluster.fork();
     }
-    var resData = '', crawl = null,
-        responseLogic = function(resData){
-            if(resData != ''){
-                req.setEncoding('utf8');
+    console.info('['+date.getTime()+'] '+numWks+' worker(s) dispatched');
+    cluster.on('exit', function(worker, code, signal) {
+        console.info('['+date.getTime()+'] worker ' + worker.process.pid + ' died.');
+    });
+
+    cluster.on('listening', function(worker, code, signal) {
+        console.log('['+date.getTime()+'] worker ' + worker.process.pid + ' is now serving.');
+    });
+
+} else {
+
+    var server = http.createServer().listen(5541, "127.0.0.1");
+    server.on('error',function(e){
+        console.error('['+date.getTime()+']' + e.message);
+    });
+    server.on('request',function(req, res){
+        date = new Date();
+        req.setEncoding('utf8');
+        var targetQuery = url.parse(req.url).query ? url.parse(req.url).query : null,
+            targetUrl = null, resData = null;
+        if(targetQuery){
+            targetUrl = queryString.parse(targetQuery).targetUrl;
+        }
+        var resData = '', crawl = null,
+            responseLogic = function(resData){
+
                 res.setHeader('Transfer-Encoding', 'chunked');
                 res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(resData);
 
-            }else{
-                res.writeHead(404);
             }
 
+        console.info('['+date.getTime()+'] Worker '+ process.pid + ' start to handle request');
+        if(targetUrl){
+            try{
+                console.info('['+date.getTime()+'] -----------------------------------------');
+                console.info('['+date.getTime()+'] Target url '+ targetUrl);
+                console.info('['+date.getTime()+'] Process started at ' + date.getTime());
+                var fileHash = crypto.createHash('md5').update(targetUrl+"").digest('hex'),
+                    filePath = configs.cacheDirectory  + fileHash + '.html';
 
-        }
+                fs.exists(filePath, function(exists) {
+                    if (exists) {
+                        console.info('['+date.getTime()+'] Cache found, start reading');
+                        var fStream = fs.createReadStream(filePath,{encoding: 'utf8'});
 
-    if(targetUrl){
-        try{
-            console.info('['+date.getTime()+'] -----------------------------------------');
-            console.info('['+date.getTime()+'] Target url '+ targetUrl);
-            console.info('['+date.getTime()+'] Process started at ' + date.getTime());
-            var fileHash = crypto.createHash('md5').update(targetUrl+"").digest('hex'),
-                filePath = configs.cacheDirectory  + fileHash + '.html';
+                        res.setHeader('Transfer-Encoding', 'chunked');
+                        res.writeHead(200, {'Content-Type': 'text/html'});
 
+                        fStream.on("data", function(data) {
+                            console.info('['+date.getTime()+'] reading data ...');
+                            res.write(data);
+                        });
+                        fStream.once("end", function() {
+                            console.info('['+date.getTime()+'] data completed.');
 
-            fs.exists(filePath, function(exists) {
-                if (exists) {
-                    console.info('['+date.getTime()+'] Cache found, start reading');
-                    resData = fs.readFileSync(filePath);
-                    responseLogic(resData);
-                } else {
+                            res.end();
+                        });
 
-                    if(spawnCount >= configs.concurrentWorkers){
-                        res.writeHead(404);
-                        res.end('');
+                    } else {
+                        try{
+                            crawl = spawn('phantomjs',['./spider/app.js',targetUrl]);
+                        }catch(e){
+                            console.error('['+date.getTime()+']' + e.message);
+                        }
+                        console.info('['+date.getTime()+'] Cache empty, starting calling crawl');
+                        console.info('['+date.getTime()+'] Processing ...');
+                        res.setHeader('Transfer-Encoding', 'chunked');
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+
+                        var writeSteam = fs.createWriteStream(filePath,{encoding: 'utf8'})
+                        crawl.stdout.setEncoding('utf8');
+                        crawl.stdout.on('data', function(data) {
+                            console.info('['+date.getTime()+'] transmitting data ...' );
+                            writeSteam.write(data);
+                            res.write(data);
+                        });
+                        crawl.stderr.on('data', function(data) {
+                            console.error('['+date.getTime()+'] ' + data);
+                        });
+                        crawl.on('close',function(){
+
+                            console.info('['+date.getTime()+'] data completed, crawler stopped' );
+                            writeSteam.close();
+                            res.end();
+
+                        });
                     }
-                    try{
-                        crawl = spawn('phantomjs',['./spider/app.js',targetUrl]);
-
-                    }catch(e){
-                        console.error('['+date.getTime()+']' + e.message);
-                    }
-                    spawnCount ++;
-                    console.info('['+date.getTime()+'] Cache empty, starting calling crawl');
-                    console.info('['+date.getTime()+'] ...Processing...');
-
-
-                    crawl.stdout.setEncoding('utf8');
-                    crawl.stdout.on('data', function(data) {
-                        console.log(data)
-                        resData += data;
-                    });
-                    crawl.stderr.on('data', function(data) {
-                        console.error('['+date.getTime()+'] ' + data);
-                    });
-                    crawl.on('close',function(){
-                        fs.writeFileSync(filePath, resData);
-                        spawnCount --;
-                        responseLogic(resData);
-
-                    });
-                }
-            });
+                });
 
 
 
-        }catch(e){
-            console.error('['+date.getTime()+']' + e.message);
+            }catch(e){
+                console.error('['+date.getTime()+']' + e.message);
+                res.writeHead(404);
+                res.end('');
+            }
+        }else{
             res.writeHead(404);
             res.end('');
         }
-    }else{
-        res.writeHead(404);
-        res.end('');
-    }
+    });
 
+}
 
-}).listen(5542, "127.0.0.1");
-
-server.on('error',function(e){
-    console.error('['+date.getTime()+']' + e.message);
-});
