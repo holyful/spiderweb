ee51ded9d26d0362cbb1ce38daf7accd53fe1c9a
达轩 佘 <holyful@hotmail.com>
2014-01-26 18:07:10 +0800

    对第三方库的依赖全部standalone

--- spider/index/hook/weixin/index.js
+++ spider/index/hook/weixin/index.js
@@ -1,12 +1,14 @@
 setTimeout(function(){
 
-/*
 	var elem = document.getElementsByClassName("deallist")[0];
 	var evt = document.createEvent("MouseEvents");
   	evt.initEvent('mousewheel', true, true);
   	evt.wheelDelta = -1000;
-  	setInterval(function(){elem.dispatchEvent(evt)},200);
-*/
+  	setInterval(function(){
+        if(elem){
+            elem.dispatchEvent(evt)
+        };
+    },200);
 
   	setInterval(function(){
   		if (typeof window.callPhantom === 'function') {
@@ -14,6 +16,6 @@ setTimeout(function(){
 		}
   	},5000);
 
-},3000);
+},30000);
 
 
